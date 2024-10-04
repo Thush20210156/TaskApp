7c74ffbdd1342c7956e8d3936e1bedddfe18a074
Thushini Abeysuriya <thushiniabeysuriya@gmail.com>
2024-10-04 16:09:54 +0530

    Initial Commit

---
+++ TaskApp/TaskAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskAppApp.swift
+//  TaskApp
+//
+//  Created by Thushini Abeysuriya on 2024-10-04.
+//
+
+import SwiftUI
+
+@main
+struct TaskAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
